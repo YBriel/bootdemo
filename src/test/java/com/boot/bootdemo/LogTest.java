be42e0961bec6a42c12ac3c873ef487687e73a60
xiegs <xiegs@0791jx.cn>
2020-05-25 15:21:24 +0800

    other features added

---
+++ src/test/java/com/boot/bootdemo/LogTest.java
@@ -0,0 +1,28 @@
+package com.boot.bootdemo;
+
+import com.boot.bootdemo.entity.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+
+/**
+ * description:
+ * author: yuzq
+ * date: 2020/5/25 14:43
+ */
+public class LogTest {
+
+    private static Logger logger= LoggerFactory.getLogger(LogTest.class);
+
+    public static void main(String[] args) {
+        logger.info("这是info");
+        logger.warn("这是warn");
+        logger.debug("这是debug");
+        logger.error("这是error");
+        logger.trace("这是trace");
+        Student student=new Student();
+        student.setAge(22);
+        student.setName("tom");
+        logger.info("ss",student.getName());
+    }
+}

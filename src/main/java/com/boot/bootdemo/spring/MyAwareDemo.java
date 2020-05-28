62c185a354bd0e97759eec9fc8e0fa4ecf3fb6ee
xiegs <xiegs@0791jx.cn>
2020-05-28 19:18:25 +0800

    other features added

--- src/main/java/com/boot/bootdemo/spring/MyAwareDemo.java
+++ src/main/java/com/boot/bootdemo/spring/MyAwareDemo.java
@@ -8,7 +8,7 @@ import jodd.util.MathUtil;
 
 /**
  * description:
- * author: yuzq
+ * author: yuzq static配合synchronized全局唯一方法锁
  * date: 2020/5/28 17:49
  */
 public class MyAwareDemo {
@@ -18,12 +18,31 @@ public class MyAwareDemo {
         return student;
     }
 
+    private synchronized static  int test(int a ,int b) throws InterruptedException {
+        int i = a + b;
+        System.out.println("a="+a+" b="+b+"  a+b="+i);
+        Thread.sleep(5000);
+         return i;
+    }
+
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+/*        for (int i = 0; i < 10; i++) {
             new Thread(() -> {
                 try {
                     MyAwareDemo.test().setAge(MathUtil.randomInt(1,22));
                     System.out.println(MyAwareDemo.test());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            },"1").start();
+        }*/
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                try {
+                    MyAwareDemo awareDemo=new MyAwareDemo();
+
+                    awareDemo.test(MathUtil.randomInt(1,22),MathUtil.randomInt(33,222));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
---
+++ src/main/java/com/boot/bootdemo/spring/MyBeanFactoryDemo1.java
@@ -0,0 +1,51 @@
+package com.boot.bootdemo.spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.AbstractBeanFactory;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.core.ResolvableType;
+
+/**
+ * @description:
+ * @author: xgs
+ * @date: 2020/5/28 18:55
+ */
+public class MyBeanFactoryDemo1 extends AbstractBeanFactory {
+    @Override
+    protected boolean containsBeanDefinition(String s) {
+        return false;
+    }
+
+    @Override
+    protected BeanDefinition getBeanDefinition(String s) throws BeansException {
+        return null;
+    }
+
+    @Override
+    protected Object createBean(String s, RootBeanDefinition rootBeanDefinition, Object[] objects) throws BeanCreationException {
+        return null;
+    }
+
+    @Override
+    public <T> T getBean(Class<T> aClass) throws BeansException {
+        return null;
+    }
+
+    @Override
+    public <T> T getBean(Class<T> aClass, Object... objects) throws BeansException {
+        return null;
+    }
+
+    @Override
+    public <T> ObjectProvider<T> getBeanProvider(Class<T> aClass) {
+        return null;
+    }
+
+    @Override
+    public <T> ObjectProvider<T> getBeanProvider(ResolvableType resolvableType) {
+        return null;
+    }
+}

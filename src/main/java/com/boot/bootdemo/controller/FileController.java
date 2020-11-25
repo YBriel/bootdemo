b107be4bf388cfd247b65d839073245925b753ca
yuzhenqing <yuzq@0791jx.cn>
2020-11-25 11:38:45 +0800

    add file upload demo

---
+++ src/main/java/com/boot/bootdemo/controller/FileController.java
@@ -0,0 +1,45 @@
+package com.boot.bootdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * author: yuzq
+ * create: 2020-11-25 11:38
+ **/
+@RestController
+public class FileController {
+
+    @RequestMapping("xls")
+    public String test(@RequestParam("file") MultipartFile file)  {
+        if (file.isEmpty()) {
+            return "文件为空";
+        }
+        // 获取文件名
+        String fileName = file.getOriginalFilename();
+        System.out.println("上传的文件名为：" + fileName);
+        // 获取文件的后缀名
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        System.out.println("上传的后缀名为：" + suffixName);
+        // 文件上传后的路径
+        String filePath = "D://";
+        File dest = new File(filePath + fileName);
+        // 检测是否存在目录
+        if (!dest.getParentFile().exists()) {
+            dest.getParentFile().mkdirs();
+        }
+        try {
+            file.transferTo(dest);
+            return "上传成功";
+        } catch (IllegalStateException | IOException e) {
+            e.printStackTrace();
+        }
+        return "上传失败";
+
+    }
+}

4a9b2452df3c8717629d54ffd0df962a74559931
yuzhenqing <yuzq@0791jx.cn>
2020-12-26 15:33:31 +0800

    add mybatis mapper

--- src/main/java/com/boot/bootdemo/mapper/StudentMapper.java
+++ src/main/java/com/boot/bootdemo/mapper/StudentMapper.java
@@ -3,8 +3,12 @@ package com.boot.bootdemo.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.boot.bootdemo.entity.Student;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * Author： yuzq
@@ -20,4 +24,10 @@ public interface StudentMapper extends BaseMapper<Student> {
 
     @Select("select * from student where id=#{id} for update")
     Student getStu(int id);
+
+
+    //in 一个list查询 使用$符号用作变量
+/*    @Results
+    @Select("SELECT * FROM order_table WHERE driver_id=#{driverId} and order_status = #{orderStatus} AND type IN (${type}) AND state = 1 ")
+    List<Student> queryOrderListByStatus(@Param("driverId") Long driverId, @Param("orderStatus") Integer orderStatus, @Param("type") String type);*/
 }

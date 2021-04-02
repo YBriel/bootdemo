e3bda66c3835d4de397427a3950db89aad352a76
yuzhenqing <yuzq@0791jx.cn>
2021-04-02 18:28:25 +0800

    completableFuture added

---
+++ src/main/java/com/boot/bootdemo/thread/completable/MyCompletableDemo.java
@@ -0,0 +1,62 @@
+package com.boot.bootdemo.thread.completable;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * author: yuzq
+ * create: 2021-04-02 15:40
+ **/
+@Slf4j
+public class MyCompletableDemo {
+
+
+    public String sayHello(){
+        log.info("你好...");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "hello";
+    }
+
+
+    public Integer guess(){
+        log.info("你好猜一下...");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return 1;
+    }
+
+    public void test(){
+        log.info("这个是打印...");
+    }
+
+
+    public CompletableFuture<String> sayHelloFuture() {
+        return CompletableFuture.supplyAsync(this::sayHello);
+    }
+
+    public CompletableFuture<Integer> guessFuture() {
+        return CompletableFuture.supplyAsync(this::guess);
+    }
+
+    public static void main(String[] args) {
+        long time=System.currentTimeMillis();
+        MyCompletableDemo demo=new MyCompletableDemo();
+        demo.sayHello();
+        demo.guess();
+        log.info("两步耗时{}",System.currentTimeMillis()-time);
+
+        long time2=System.currentTimeMillis();
+        CompletableFuture<String> stringCompletableFuture = demo.sayHelloFuture();
+        CompletableFuture<Integer> integerCompletableFuture = demo.guessFuture();
+        CompletableFuture.allOf(stringCompletableFuture,integerCompletableFuture).thenRun(() -> System.out.println("完成！！！！")).join();
+        log.info("两步耗时{}",System.currentTimeMillis()-time2);
+    }
+}

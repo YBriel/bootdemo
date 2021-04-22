c5873305d5f48ecf2fb258124c076bec9958f67f
yuzq <1065265077@qq.com>
2021-04-22 08:00:38 +0800

    interface

--- src/main/java/com/boot/bootdemo/java8/generic/PayNotifyService.java
+++ src/main/java/com/boot/bootdemo/java8/generic/PayNotifyService.java
@@ -1,13 +1,24 @@
 package com.boot.bootdemo.java8.generic;
 
+import lombok.Data;
+
 /**
  * author: yuzq
  * create: 2021-04-18 09:43
  **/
+@Data
 public class PayNotifyService implements TLNotifyService<PayNotify> {
+
+    private String name;
 
     @Override
     public PayNotify gen(String content) {
         return null;
+    }
+
+    public static void main(String[] args) {
+        TLNotifyService<PayNotify> service=new PayNotifyService();
+        PayNotifyService service1 = (PayNotifyService) service;
+        String name = service1.name;
     }
 }

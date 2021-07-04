5bacb7d21dbec4b0e1c0696869ca255c72360cc0
余振清 <1065265077@qq.com>
2021-07-04 15:08:13 +0800

    add null integer to bigdecimal string demo

--- src/main/java/com/boot/bootdemo/entity/money/MyBigDecimalDemo.java
+++ src/main/java/com/boot/bootdemo/entity/money/MyBigDecimalDemo.java
@@ -1,9 +1,11 @@
 package com.boot.bootdemo.entity.money;
 
+import com.alibaba.fastjson.JSONObject;
 import com.boot.bootdemo.config.serilize.MyBigDecimalSerializable;
 import com.boot.bootdemo.config.serilize.MyIntegerSerializable;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
+import netscape.javascript.JSObject;
 
 import java.math.BigDecimal;
 
@@ -18,4 +20,13 @@ public class MyBigDecimalDemo {
 
     @JsonSerialize(using = MyIntegerSerializable.class,nullsUsing = MyIntegerSerializable.class)
     private Integer age;
+
+    public static void main(String[] args) {
+        MyBigDecimalDemo decimalDemo=new MyBigDecimalDemo();
+        decimalDemo.setName("tom");
+        decimalDemo.setAge(12);
+        decimalDemo.setMoney(BigDecimal.TEN);
+
+        System.out.println(JSONObject.toJSONString(decimalDemo));
+    }
 }
